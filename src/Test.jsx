00394830712954cd9613d61cd5b63b25cceaa099
yichien <kevinyichienlee@gmail.com>
2023-10-21 23:41:01 +0800

    framer practice animations

--- src/Test.jsx
+++ src/Test.jsx
@@ -3,25 +3,35 @@ import { motion } from "framer-motion";
 
 const Test = () => {
   const variants = {
-    visible: {
+    visible: (i) => ({
       opacity: 1,
-      x: 500,
-      transition: { type: "spring", stiffness: 100, damping: 100 },
-    },
+      x: -100,
+      transition: { delay: i * 0.3 },
+      // transition: { staggerChildren: 0.2 },
+    }),
     hidden: { opacity: 0 },
   };
 
   const [open, setOpen] = useState();
 
+  const items = ["i1", "i2", "i3", "i4"];
+
   return (
     <div className="course">
-      <motion.div
+      {/* <motion.div
         className="box"
         variants={variants}
         animate={open ? "visible" : "hidden"}
         drag
       ></motion.div>
-      <button onClick={() => setOpen((prev) => !prev)}>Click</button>
+      <button onClick={() => setOpen((prev) => !prev)}>Click</button> */}
+      <motion.ul initial="hidden" animate="visible" variants={variants}>
+        {items.map((item, idx) => (
+          <motion.li variants={variants} key={item} custom={idx}>
+            {item}
+          </motion.li>
+        ))}
+      </motion.ul>
     </div>
   );
 };
